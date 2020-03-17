f690f472c91758d2e2f686fa48a0d8b12fb03f03
Timothy Redaelli <tredaelli@redhat.com>
2020-03-17 17:48:33 +0100

    Use l'url corretto per _data

--- _plugins/hook_site_after_init.rb
+++ _plugins/hook_site_after_init.rb
@@ -2,5 +2,5 @@ require 'fileutils'
 
 Jekyll::Hooks.register :site, :after_init do |site|
     FileUtils.rm_r '_data/', force: true
-    system("git clone --depth 1 https://github.com/drizzt/covid19italia_opendata _data/")
+    system("git clone --depth 1 https://github.com/emergenzeHack/covid19italia_data _data/")
 end
