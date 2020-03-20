02a3e2a0f53a4ea609f0c049e116c8f8e6884f63
Antonio Vivace <avivace4@gmail.com>
2020-03-20 01:02:31 +0100

    Considera solo le segnalazioni accettate nei conteggi

--- scripts/aggiornaStatistiche.py
+++ scripts/aggiornaStatistiche.py
@@ -12,13 +12,14 @@ repoSegnalazioni = g.get_repo("emergenzeHack/covid19italia_segnalazioni")
 labels = repoSegnalazioni.get_labels()
 data = []
 data.append({ 'Tipo': "Segnalazioni totali", 'Valore': repoSegnalazioni.get_issues().totalCount })
+data.append({ 'Tipo': "Accettate", 'Valore': repoSegnalazioni.get_issues(labels=["accettato"]).totalCount })
 
 for label in labels:
-	print(label.name)
-	count = repoSegnalazioni.get_issues(labels=[label.name]).totalCount
-	if count != 0:
-		data.append({ 'Tipo': label.name,
-					  'Valore': count} )
+	if label not in ["accettato"]:
+		count = repoSegnalazioni.get_issues(labels=["accettato", label.name]).totalCount
+		if count != 0:
+			data.append({ 'Tipo': label.name,
+						  'Valore': count} )
 
 with open(os.path.dirname(__file__) + '/../_data/statisticheSegnalazioni.csv', 'w') as csv_file:
 	fieldnames = ["Tipo", "Valore"]
