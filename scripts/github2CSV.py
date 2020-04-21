4800a512a0ed532561d0bb1908bce7c801f48099
Davide Monfrecola <davide.monfrecola@adevinta.com>
2020-04-21 22:54:39 +0200

    Applying some pylint suggestions

--- scripts/github2CSV.py
+++ scripts/github2CSV.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python
 
-from github import Github
-from pathlib import Path
-import datetime
-import csv
-import sys
-from lxml import html
-import json
-import yaml
 import configparser
-import os
+import csv
+import datetime
 import io
+import json
 import logging
-
-import pandas as pd
+import os
+import sys
+from pathlib import Path
+import yaml
 import geopandas as gpd
+import pandas as pd
+
+from github import Github
+from lxml import html
 from shapely.geometry import Point
 
 # Configure logger to print log message to stdout
@@ -22,31 +22,31 @@ logformat = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 logging.basicConfig(stream=sys.stdout, level=logging.INFO, format=logformat)
 logger = logging.getLogger('github2CSV')
 
-CSV_COLUMN_NAMES = ["url","id","updated_at","created_at","title","lat","lon","regione","provincia","labels","milestone","image","data","body","state"]
+CSV_COLUMN_NAMES = ["url", "id", "updated_at", "created_at", "title", "lat", "lon", "regione", "provincia", "labels", "milestone", "image", "data", "body", "state"]
 
 # Program arguments
-CSVFILE=sys.argv[1]
+CSVFILE = sys.argv[1]
 try:
-    JSONFILE=sys.argv[2]
-    jwr=io.open(JSONFILE,"r+",encoding="utf-8")
+    JSONFILE = sys.argv[2]
+    jwr = io.open(JSONFILE, "r+", encoding="utf-8")
 except:
-    jwr=None
-
-try:
-    GEOJSONFILE=sys.argv[3]
-    gjwr=io.open(GEOJSONFILE,"r+",encoding="utf-8")
-except:
-    gjwr=None
-
-LIMITIPATH=sys.argv[4]
+    jwr = None
 
 try:
-    ACCETTATOLABEL=sys.argv[5]
+    GEOJSONFILE = sys.argv[3]
+    gjwr = io.open(GEOJSONFILE, "r+", encoding="utf-8")
 except:
-    ACCETTATOLABEL="Accettato"
+    gjwr = None
 
-FILTER_LABELS=("Accettato","accepted")
-POSIZIONE_NAMES=("posizione","Posizione","position","Position","location","Location")
+LIMITIPATH = sys.argv[4]
+
+try:
+    ACCETTATOLABEL = sys.argv[5]
+except:
+    ACCETTATOLABEL = "Accettato"
+
+FILTER_LABELS = ("Accettato", "accepted")
+POSIZIONE_NAMES = ("posizione", "Posizione", "position", "Position", "location", "Location")
 
 TMPCSVFILE = '../_data/issues_temp.csv'
 TMPJSONFILE = '../_data/issuesjson_temp.json'
@@ -55,23 +55,23 @@ TMPGEOJSONFILE = '../_data/issues_temp.geojson'
 # Default values for repository name and Github organization
 # They are used if an error occurred while reading values
 # from configuration file
-REPO_NAME='covid19italia_segnalazioni'
-ORG='emergenzeHack'
+REPO_NAME = 'covid19italia_segnalazioni'
+ORG = 'emergenzeHack'
 
 def get_github_client():
     try:
-        config=configparser.RawConfigParser()
+        config = configparser.RawConfigParser()
         config.read('.github.cfg')
 
-        TOKEN=None
-        PASS=config.get('GitHub','TOKEN')
-        USER=config.get('GitHub','USER')
-        REPO_NAME=config.get('GitHub','REPO_NAME')
-        ORG=config.get('GitHub','ORG')
+        TOKEN = None
+        PASS = config.get('GitHub', 'TOKEN')
+        USER = config.get('GitHub', 'USER')
+        REPO_NAME = config.get('GitHub', 'REPO_NAME')
+        ORG = config.get('GitHub', 'ORG')
     except:
-        TOKEN=os.environ.get('GITHUB_TOKEN')
-        PASS=os.environ.get('GITHUB_PASSWORD')
-        USER=os.environ.get('GITHUB_USERNAME')
+        TOKEN = os.environ.get('GITHUB_TOKEN')
+        PASS = os.environ.get('GITHUB_PASSWORD')
+        USER = os.environ.get('GITHUB_USERNAME')
 
     if not TOKEN:
         if not PASS:
@@ -105,7 +105,7 @@ def get_latest_timestamp(csvfile):
         max_updated_at = max(data['updated_at'][1:-1])
         return datetime.datetime.strptime(max_updated_at, '%Y-%m-%d %H:%M:%S')
 
-    return datetime.datetime(2000,1,1)
+    return datetime.datetime(2000, 1, 1)
 
 def write_output_files(issues):
     write_csv_file(issues)
@@ -124,11 +124,11 @@ def write_csv_file(issues):
         for line in csvreader:
             issue_id = int(line[1])
             if issue_id in issues:
-                # the issue has been updated, we need to update it in our CSV file 
+                # the issue has been updated, we need to update it in our CSV file
                 issue = issues[issue_id]
                 gh_issue = issue["issue"] # Github issue instance
-                logger.info("[CSV] Updating issue {}...".format(issue_id))
-                row = (gh_issue.html_url,issue_id,gh_issue.updated_at,gh_issue.created_at,issue["title"],issue["lat"],issue["lon"],issue["regioneIssue"],issue["provinciaIssue"],issue["labels"],gh_issue.milestone,issue["image"],json.dumps(issue["data"],sort_keys=True),gh_issue.body, gh_issue.state)
+                logger.info("[CSV] Updating issue %d...", issue_id)
+                row = get_csv_issue(issue, gh_issue)
                 del issues[issue_id]
             else:
                 # otherwise, just append the existing row without modifying it
@@ -139,7 +139,7 @@ def write_csv_file(issues):
         for issue_id in issues: # append the remaining new issues
             issue = issues[issue_id]
             gh_issue = issue["issue"] # Github issue instance
-            row = (gh_issue.html_url,issue_id,gh_issue.updated_at,gh_issue.created_at,issue["title"],issue["lat"],issue["lon"],issue["regioneIssue"],issue["provinciaIssue"],issue["labels"],gh_issue.milestone,issue["image"],json.dumps(issue["data"],sort_keys=True),gh_issue.body, gh_issue.state)
+            row = get_csv_issue(issue, gh_issue)
             csvwriter.writerow(row)
     # move temp file to final one
     Path(TMPCSVFILE).rename(CSVFILE)
@@ -154,20 +154,20 @@ def write_json_file(issues):
                 # the issue has been updated, we need to update it in our JSON file
                 issue = issues[issue_id]
                 gh_issue = issue["issue"]
-                logger.info("[JSON] Updating issue {}...".format(issue_id))
+                logger.info("[JSON] Updating issue %d...", issue_id)
                 jsonarray.append(get_json_issue(issue, gh_issue))
                 del issues[issue_id]
             else:
                 # otherwise, just append the existing row without modifying it
                 jsonarray.append(row)
-         
+
         logger.info("[JSON] Writing new issues...")
         for issue_id in issues: # append the remaining new issues
             issue = issues[issue_id]
             gh_issue = issue["issue"]
             jsonarray.append(get_json_issue(issue, gh_issue))
 
-        output_file.write(json.dumps(jsonarray,ensure_ascii=False,sort_keys=True))
+        output_file.write(json.dumps(jsonarray, ensure_ascii=False, sort_keys=True))
     # move temp file to final one
     Path(TMPJSONFILE).rename(JSONFILE)
 
@@ -182,13 +182,13 @@ def write_geojson_file(issues):
                 # the issue has been updated, we need to update it in our JSON file
                 issue = issues[issue_id]
                 gh_issue = issue["issue"]
-                logger.info("[GeoJSON] Updating issue {}...".format(issue_id))
+                logger.info("[GeoJSON] Updating issue %d...", issue_id)
                 geojsonarray.append(get_geojson_issue(issue, gh_issue))
                 del issues[issue_id]
             else:
                 # otherwise, just append the existing row without modifying it
                 geojsonarray.append(row)
-        
+
         logger.info("[GeoJSON] Writing new issues...")
         for issue_id in issues: # append the remaining new issues
             issue = issues[issue_id]
@@ -196,10 +196,17 @@ def write_geojson_file(issues):
             geojsonarray.append(get_geojson_issue(issue, gh_issue))
 
         output_file.write(str('{ "type": "FeatureCollection", "features": '))
-        output_file.write(json.dumps(geojsonarray,ensure_ascii=False,sort_keys=True)+"}")
+        output_file.write(json.dumps(geojsonarray, ensure_ascii=False, sort_keys=True) + "}")
     # move temp file to final one
     Path(TMPGEOJSONFILE).rename(GEOJSONFILE)
-    
+
+def get_csv_issue(issue, gh_issue):
+    return (
+        gh_issue.html_url, issue["id"], gh_issue.updated_at, gh_issue.created_at,
+        issue["title"], issue["lat"], issue["lon"], issue["regioneIssue"], issue["provinciaIssue"],
+        issue["labels"], gh_issue.milestone, issue["image"], json.dumps(issue["data"], sort_keys=True),
+        gh_issue.body, gh_issue.state
+    )
 
 def get_json_issue(issue, gh_issue):
     return {
@@ -209,8 +216,8 @@ def get_json_issue(issue, gh_issue):
         "issue":{
             "url":gh_issue.html_url,
             "id":gh_issue.id,
-            "updated_at":gh_issue.updated_at.isoformat()+"+00:00",
-            "created_at":gh_issue.created_at.isoformat()+"+00:00",
+            "updated_at":gh_issue.updated_at.isoformat() + "+00:00",
+            "created_at":gh_issue.created_at.isoformat() + "+00:00",
             "title":issue["title"],
             "lat":issue["lat"],
             "lon":issue["lon"],
@@ -228,39 +235,39 @@ def get_geojson_issue(issue, gh_issue):
         "type":"Feature",
         "geometry":{
             "type":"Point",
-            "coordinates":[issue["lon"],issue["lat"]]},
-            "properties":{
-                "title":gh_issue.title,
-                "number":gh_issue.number,
-                "state":gh_issue.state,
-                "url":gh_issue.html_url,
-                "id":gh_issue.id,
-                "updated_at":gh_issue.updated_at.isoformat()+"+00:00",
-                "created_at":gh_issue.created_at.isoformat()+"+00:00",
-                "labels":eval(issue["labels"]) if issue["labels"] else None,
-                "milestone":gh_issue.milestone.title if gh_issue.milestone else None,
-                "image":image,
-                "data":issue["data"],
-                "body":gh_issue.body,
-                "regione":issue["regioneIssue"],
-                "provincia":issue["provinciaIssue"]
+            "coordinates":[issue["lon"], issue["lat"]]},
+            "properties": {
+                "title": gh_issue.title,
+                "number": gh_issue.number,
+                "state": gh_issue.state,
+                "url": gh_issue.html_url,
+                "id": gh_issue.id,
+                "updated_at": gh_issue.updated_at.isoformat() + "+00:00",
+                "created_at": gh_issue.created_at.isoformat() + "+00:00",
+                "labels": eval(issue["labels"]) if issue["labels"] else None,
+                "milestone": gh_issue.milestone.title if gh_issue.milestone else None,
+                "image": image,
+                "data": issue["data"],
+                "body": gh_issue.body,
+                "regione": issue["regioneIssue"],
+                "provincia": issue["provinciaIssue"]
             }
         }
 
 if __name__ == "__main__":
     logger.info("Reading 'regioni' geo data file...")
-    regioni=gpd.read_file(LIMITIPATH+"/Limiti01012019_g/Reg01012019_g/Reg01012019_g_WGS84.shp")
+    regioni = gpd.read_file(LIMITIPATH + "/Limiti01012019_g/Reg01012019_g/Reg01012019_g_WGS84.shp")
 
-    regioni=gpd.GeoDataFrame(regioni)
-    regioni.crs='epsg:23032'
-    regioni=regioni.to_crs('epsg:4326')
+    regioni = gpd.GeoDataFrame(regioni)
+    regioni.crs = 'epsg:23032'
+    regioni = regioni.to_crs('epsg:4326')
 
     logger.info("Reading 'province' geo data file...")
-    province=gpd.read_file(LIMITIPATH+"/Limiti01012019_g/ProvCM01012019_g/ProvCM01012019_g_WGS84.shp")
+    province = gpd.read_file(LIMITIPATH + "/Limiti01012019_g/ProvCM01012019_g/ProvCM01012019_g_WGS84.shp")
 
-    province=gpd.GeoDataFrame(province)
-    province.crs='epsg:23032'
-    province=province.to_crs('epsg:4326')
+    province = gpd.GeoDataFrame(province)
+    province.crs = 'epsg:23032'
+    province = province.to_crs('epsg:4326')
 
     logger.info("Reading Github configration...")
     g = get_github_client()
@@ -268,66 +275,67 @@ if __name__ == "__main__":
     org = g.get_organization(ORG)
     r = org.get_repo(REPO_NAME)
 
-    filter_labels=[]
+    # Retrieve filter labels from Github repository
+    filter_labels = []
     for l in FILTER_LABELS:
         try:
-            ghlabel=r.get_label(l)
+            ghlabel = r.get_label(l)
             filter_labels.append(ghlabel)
         except:
             pass
 
-    logger.info("Retrieving latest updated_at timestamp from our issues file ({0})...".format(CSVFILE))
-    latestTimestamp = get_latest_timestamp(CSVFILE)
+    logger.info("Retrieving latest updated_at timestamp from our issues file (%s)...", CSVFILE)
+    latest_timestamp = get_latest_timestamp(CSVFILE)
     # we need to add one second to the latest timestamp in our issue file
     # to avoid retrieving the "last" issue we already have (in the issues file)
-    lastTime = latestTimestamp + datetime.timedelta(seconds=1)
+    last_time = latest_timestamp + datetime.timedelta(seconds=1)
 
-    logger.info("Retrieving issues from Github (since {0})...".format(lastTime))
-    issues=r.get_issues(since=lastTime,labels=filter_labels,state='all',sort='updated')
-    logger.info("{0} issues retrieved...".format(issues.totalCount))
+    logger.info("Retrieving issues from Github (since %s)...", last_time)
+    issues = r.get_issues(since=last_time, labels=filter_labels, state='all', sort='updated')
+    logger.info("%d issues retrieved...", issues.totalCount)
 
-    issuedict={}
-    geojsonarray=[]
+    issuedict = {}
+    geojsonarray = []
 
     for issue in issues:
         labels = json.dumps([l.name for l in issue.labels])
-        data={}
+        data = {}
         lat = lon = image = regioneIssue = provinciaIssue = None
 
         try:
-            tree=html.fromstring(issue.body)
+            tree = html.fromstring(issue.body)
 
             try:
-                dataRaw=tree.xpath("//data/text()")
-                dataStr=dataRaw[0] if len(dataRaw) > 0 else None
-                data=json.loads(dataStr)
+                dataRaw = tree.xpath("//data/text()")
+                dataStr = dataRaw[0] if len(dataRaw) > 0 else None
+                data = json.loads(dataStr)
             except:
                 pass
 
             try:
-                yamldataRaw=tree.xpath("//yamldata/text()")
-                yamldataStr=yamldataRaw[0] if len(yamldataRaw) > 0 else None
-                data=yaml.safe_load(yamldataStr)
+                yamldataRaw = tree.xpath("//yamldata/text()")
+                yamldataStr = yamldataRaw[0] if len(yamldataRaw) > 0 else None
+                data = yaml.safe_load(yamldataStr)
             except:
                 pass
         except:
             pass
 
         if not data:
-            logger.info("Data not found for issue {issue}.".format(issue=issue))
+            logger.info("Data not found for issue %s.", issue)
             continue
 
         for posName in POSIZIONE_NAMES:
             if posName in data:
                 try:
-                    (lat,lon) = data[posName].split(" ")[:2]
-                    p = Point(float(lon),float(lat))
-                    for i,regione in regioni.iterrows():
+                    (lat, lon) = data[posName].split(" ")[:2]
+                    p = Point(float(lon), float(lat))
+                    for i, regione in regioni.iterrows():
                         if regione['geometry'].contains(p):
                             regioneIssue = regione["DEN_REG"]
                             break
 
-                    for i,provincia in province.iterrows():
+                    for i, provincia in province.iterrows():
                         if provincia['geometry'].contains(p):
                             provinciaIssue = provincia["DEN_UTS"]
                             break
@@ -343,13 +351,9 @@ if __name__ == "__main__":
             provinciaIssue = data["provincia_manuale"]
 
         if "immagine" in data:
-            image=data['immagine']
+            image = data['immagine']
 
-        title=issue.title
-        if title is not None:
-            title=title
-
-        labels=labels
+        title = issue.title
 
         issuedict[issue.id] = {
             "issue": issue,
